5f426e2aa1cf630f2865430bf3b1c43c7c2e690f
xuanbai.shen <weridolm@qq.com>
2021-08-23 16:36:01 +0800

    加入计划模块

---
+++ src/main/java/cn/hamster3/bot/preset/schedule/HourSchedule.java
@@ -0,0 +1,24 @@
+package cn.hamster3.bot.preset.schedule;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class HourSchedule {
+    public static void hour() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 12); // 控制时
+        calendar.set(Calendar.MINUTE, 0);       // 控制分
+        calendar.set(Calendar.SECOND, 0);       // 控制秒
+
+        Date time = calendar.getTime();         // 得出执行任务的时间,此处为今天的12：00：00
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                System.out.println("-------设定要指定任务--------");
+            }
+        }, time, 1000 * 60 * 60 * 24);// 这里设定将延时每天固定执行
+    }
+}
